3665d1683f35301a380b2f98788786d9d47eb9d9
nggiest <gioanggiesta@gmail.com>
2022-05-15 19:21:36 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavigationBar from './components/NavigationBar';
+
+function App() {
+  return (
+   <div>
+     <NavigationBar/>
+   </div>
+  );
+}
+
+export default App;
